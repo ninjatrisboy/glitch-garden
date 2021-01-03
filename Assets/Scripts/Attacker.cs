40eeaefcc9c3cf545cc37b80a8151f72ee410a0b
Tristan Newball <ninjatrisboy@gmail.com>
2021-01-03 13:01:23 -0600

    Making Enemy Move
    In this one we make the enemy move by using     transform.translate       WE can make the enemy move FAST or sssllllooooowwwwww.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour
-{
+public class Attacker : MonoBehaviour{
+
+    [Range(0f, 5f)]
+    [SerializeField] float walkSpeed = 1f;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start()    {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        transform.Translate(Vector2.left * walkSpeed* Time.deltaTime);
     }
 }
