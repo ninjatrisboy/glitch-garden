50c032d2397f6807a728549e269be4d6c6830772
Tristan Newball <ninjatrisboy@gmail.com>
2021-01-04 11:23:23 -0600

    Make projectile hurt enemys
    Now the projectile can hurt the lzards and kill them.Next we make death VFX.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,9 +6,17 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField ]float Speed = 5f;
+    [SerializeField] float damage = 100;
 
     void Update()
     {
         transform.Translate(Vector2.right * Speed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+     {
+         var health = otherCollider.GetComponent<Health>();
+         health.DealDamage(damage);
+     }
+
 }
