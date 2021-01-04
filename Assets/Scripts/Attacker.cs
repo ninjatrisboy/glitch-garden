a2782c79a425601795c54c52d8e81fe6089cbc9b
Tristan Newball <ninjatrisboy@gmail.com>
2021-01-03 18:31:46 -0600

    Spawning attackers and making them not glitch
    We made a spawning attacker with corutines and we made it not mave the portal and now we make a defender.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour{
+public class Attacker : MonoBehaviour
+{
 
     [Range(0f, 5f)]
-    [SerializeField] float walkSpeed = 1f;
+    float currentSpeed = 1f;
 
-
-    // Start is called before the first frame update
-    void Start()    {
-        
+    void Update()
+    {
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update() {
-        transform.Translate(Vector2.left * walkSpeed* Time.deltaTime);
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
+
 }
