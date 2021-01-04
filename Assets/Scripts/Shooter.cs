5da5fc4401001046b00de8387a8126dc8f5e8506
Tristan Newball <ninjatrisboy@gmail.com>
2021-01-04 09:38:16 -0600

    Cactus
    We have made a cactus animated and are making him shoot now.He will be a defender

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour{
+
+    [SerializeField] GameObject projectile;
+
+    public void Fire()
+    {
+       Instantiate(projectile, transform.position, transform.rotation);
+    }
+
+}
