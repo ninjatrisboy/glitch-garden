463263c267184f8626e682cb7bba749a0d28d7bf
Tristan Newball <ninjatrisboy@gmail.com>
2021-01-05 07:54:18 -0600

    Cactus Spawn
    We made the cactus spawn and i got a little ahead so i made part of the next section

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+
+{
+
+    [SerializeField] GameObject cactusDefender;
+
+    private void OnMouseDown()
+
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return (gridPos);
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWoldPos)
+
+    {
+        float newX = Mathf.RoundToInt(rawWoldPos.x);
+        float newY = Mathf.RoundToInt(rawWoldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+
+
+    private void SpawnDefender(Vector2 roundedPos)
+
+    {
+        GameObject newDefender = Instantiate(cactusDefender, roundedPos, Quaternion.identity) as GameObject;
+        Debug.Log(roundedPos);
+    }
+}
