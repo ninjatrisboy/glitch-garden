ea9fc6ef9dee8f8c66b7b93cf1c7e6d18819eb17
Tristan Newball <ninjatrisboy@gmail.com>
2021-02-17 08:21:11 -0600

    BIG COMMIT
    Havent commitid in a while see if this will fix my work

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -1,48 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class DefenderSpawner : MonoBehaviour
-
 {
-
-    GameObject defender;
+    Defender defender;
 
     private void OnMouseDown()
-
     {
-        SpawnDefender(GetSquareClicked());
+        AttemptToPlaceDefenderAt(GetSquareClicked());
     }
 
-    public void SetSelectedDefender(GameObject selectedDefender)
+    public void SetSelectedDefender(Defender defenderToSelect)
     {
-        defender = selectedDefender;
+        defender = defenderToSelect;
     }
 
+    private void AttemptToPlaceDefenderAt(Vector2 gridPos)
+    {
+        Debug.Log("Is defender null? " + (defender == null));
+        var StarDisplay = FindObjectOfType<StarDisplay>();
+        int defenderCost = defender.GetStarCost();
+
+        if (StarDisplay.HaveEnoughStars(defenderCost))
+        {
+            SpawnDefender(gridPos);
+            StarDisplay.SpendStars(defenderCost);
+        }
+    }
 
     private Vector2 GetSquareClicked()
-
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
         Vector2 gridPos = SnapToGrid(worldPos);
-        return (gridPos);
+        return gridPos;
     }
-
-    private Vector2 SnapToGrid(Vector2 rawWoldPos)
-
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
     {
-        float newX = Mathf.RoundToInt(rawWoldPos.x);
-        float newY = Mathf.RoundToInt(rawWoldPos.y);
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
         return new Vector2(newX, newY);
     }
-
-
-
     private void SpawnDefender(Vector2 roundedPos)
-
     {
-        GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
         Debug.Log(roundedPos);
     }
 }
--- Assets/Scripts/StarDisplay.cs
+++ Assets/Scripts/StarDisplay.cs
@@ -1,23 +1,24 @@
-﻿ using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
- using UnityEngine.UI;
-
- public class StarDisplay : MonoBehaviour
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StarDisplay : MonoBehaviour
 {
-
-    [SerializeField] int stars = 100;
+    [SerializeField] int stars = 300;
     Text starText;
-
     void Start()
     {
         starText = GetComponent<Text>();
         UpdateDisplay();
     }
-
     private void UpdateDisplay()
     {
         starText.text = stars.ToString();
+    }
+
+    public bool HaveEnoughStars(int amount)
+    {
+        return stars >= amount;
     }
 
     public void AddStars(int amount)
@@ -25,7 +26,6 @@
         stars += amount;
         UpdateDisplay();
     }
-
     public void SpendStars(int amount)
     {
         if (stars >= amount)
