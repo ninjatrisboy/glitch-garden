065085df375446c2555cacfe70159e57c43e12d9
Tristan Newball <ninjatrisboy@gmail.com>
2021-02-17 08:22:25 -0600

    Revert "Revert "BIG COMMIT""
    This reverts commit ca9c98b458f9d82a58cd89c946aa239c763d7e13.

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DefenderButton : MonoBehaviour{
-
-    [SerializeField] GameObject defenderPrefab;
+public class DefenderButton : MonoBehaviour
+{
+    
+    [SerializeField] Defender defenderPrefab;
 
     private void OnMouseDown()
     {
+
         var buttons = FindObjectsOfType<DefenderButton>();
         foreach (DefenderButton button in buttons)
         {
-            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255); 
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
         }
 
         GetComponent<SpriteRenderer>().color = Color.white;
